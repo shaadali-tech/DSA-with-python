3bd5efdaaf1b2189d81fb0e79c88fd612994b48b
shaadali-tech <215ics043@gbu.ac.in>
2026-05-17 22:20:15 +0530

    array

---
+++ larg.py
@@ -0,0 +1,8 @@
+larg=[1,2,4,5,62,5,8]
+
+largest=larg[0]
+
+for i in larg:
+    if i>largest:
+        largest=i
+print(largest)
---
+++ largestarray.py
@@ -0,0 +1,8 @@
+array=[1,5,7,9,6,4,3]
+array.sort()
+print(array)
+
+
+largest=array[-1]
+print(largest)
+
