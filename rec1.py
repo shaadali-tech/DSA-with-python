053399e29dd221e4f0a0efcdb002743720ab97ad
shaadali-tech <215ics043@gbu.ac.in>
2026-07-11 09:53:30 +0530

    update

--- rec1.py
+++ rec1.py
@@ -11,10 +11,25 @@
 
 # Recusrsion using parameter
 
-def func(x,n):
-  if(n==0):
-    return
-  print(x)
-  return func(x,n-1)
+# def func(x,n):
+#   if(n==0):
+#     return
+#   print(x)
+#   return func(x,n-1)
 
-func(7,4)
+# func(7,4)
+
+# def fun(sum,n):
+#   if(n<0):
+#     return sum
+#   sum=sum+n
+#   return fun(sum,n-1)
+  
+# print(fun(0,10))
+
+def func(n):
+  if(n==1):
+    return 1
+  return n+func(n-1)
+
+print(func(4))
