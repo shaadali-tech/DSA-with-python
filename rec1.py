a9334df5b8b6049d2f3c0547872072643178f68e
shaadali-tech <215ics043@gbu.ac.in>
2026-07-11 10:18:44 +0530

    update

--- rec1.py
+++ rec1.py
@@ -27,9 +27,26 @@
   
 # print(fun(0,10))
 
-def func(n):
-  if(n==1):
-    return 1
-  return n+func(n-1)
+# def func(n):
+#   if(n==1):
+#     return 1
+#   return n+func(n-1)
 
-print(func(4))
+# print(func(4))
+
+# def factorial(n):
+#   if(n==0 or n==1):
+#     return 1
+#   return n*factorial(n-1)
+
+# print(factorial(9))
+
+nums=[1,2,3,4,5,6,7,8]
+
+
+i=len(nums)-1
+while(i>=0):
+  print(nums[i],end="")
+  i-=1
+
+
