77e811c952fdc8274a6aa155d122a54e8d9d9470
shaadali-tech <215ics043@gbu.ac.in>
2026-07-15 12:11:51 +0530

    update

---
+++ secondlargest.py
@@ -0,0 +1,126 @@
+# arr=[2,4,1,5,6,3,6,9,1,8]
+
+# largest=arr[0]
+# second=largest
+# n=len(arr)
+# for i in range(1,n):
+#   if(arr[i]>largest):
+#     second=largest
+#     largest=arr[i]
+#   elif arr[i] > second and arr[i] != largest:
+#         second = arr[i]
+
+
+# print(largest)
+# print(second)
+
+# arr=[1,2,3,4,5,6]
+
+# for i in range(0,len(arr)-1):
+#   if(arr[i]>arr[i+1]):
+#     print("False")
+
+    
+# unique array elements
+
+# freq_map={}
+
+# for i in range(0,n):
+#   freq_map[arr[i]]=0
+
+# print(freq_map)
+
+# j=0
+
+# for k in freq_map:
+#   arr[j]=k
+#   j+=1
+
+# print(arr)
+
+
+
+# arr=[1,1,1,2,2,2,3,3,4,4,5,5,6,6]
+
+
+
+# def unique(arr):
+#   n=len(arr)
+#   i=0
+#   j=i+1
+#   while(j<n):
+#     if(arr[i]==arr[j]):
+#       j+=1
+#     elif(arr[i]!=arr[j]):
+#       i+=1
+#       arr[i],arr[j]=arr[j],arr[i]
+#       j+=1
+#   return i+1
+
+
+# print(unique(arr))
+
+# RIGHT ROTATE AN ARRAY
+# arr=[1,2,3,4,5,6,7,8]
+
+# n=len(arr)
+# last=arr[n-1]
+
+
+# for i in range(n-1,0,-1):
+#   arr[i]=arr[i-1]
+
+# arr[0]=last
+# print(arr)
+
+# LEFT ROTATE AN ARRAY
+
+# arr=[1,2,3,4,5,6,7,8]
+
+# n=len(arr)
+# first=arr[0]
+
+# for i in range(0,n-1):
+#   arr[i]=arr[i+1]
+
+# arr[n-1]=first
+# print(arr)
+
+# RIGHT ROTATE AN ARRAY BY K PLACES
+
+
+# n=len(arr)
+# last=arr[n-1]
+
+# arr=[1,2,3,4,5,6,7,8]
+# k=3
+# n=len(arr)
+
+
+# while(k>0):
+#   last=arr[n-1]
+#   for i in range(n-1,0,-1):
+#     arr[i]=arr[i-1]
+ 
+#   arr[0]=last
+#   k-=1
+
+# print(arr)
+
+arr=[1,0,2,3,0,0,5,3,5,6]
+n=len(arr)
+for i in range(0,n):
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
